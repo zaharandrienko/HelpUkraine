74044ac3cbf197af00534a3e8effb449b1f8b787
Zahar Andrienko <zaharandrienko88@gmail.com>
2022-04-27 14:00:43 +0300

    initial commit

---
+++ text.js
@@ -0,0 +1,10 @@
+const textArr = {
+    'title' : {
+        'ua' : 'Переклад',
+        'en' : 'Translation',
+    },
+    'title2' : {
+        'ua' : 'Переклад2',
+        'en' : 'Translation2',
+    },
+}
